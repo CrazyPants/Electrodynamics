23d652e8aeae995a4466fff6a709f7d470a98e87
dmillerw <dmillerw@gmail.com>
2014-05-22 19:34:46 -0700

    Sieve logically dumps processed contents

--- src/main/java/com/edxmod/electrodynamics/common/tile/TileSieveTable.java
+++ src/main/java/com/edxmod/electrodynamics/common/tile/TileSieveTable.java
@@ -4,8 +4,12 @@ import com.edxmod.electrodynamics.common.recipe.RecipeManager;
 import com.edxmod.electrodynamics.common.recipe.manager.SieveManager;
 import com.edxmod.electrodynamics.common.util.InventoryHelper;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntityHopper;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -15,11 +19,11 @@ import java.util.Random;
 /**
  * @author Royalixor
  */
-public class TileSieveTable extends TileCore {
+public class TileSieveTable extends TileCore implements ISidedInventory {
 
-	private SieveManager.SieveRecipe processingRecipe;
+	private static final int INVENTORY_SIZE = 9;
 
-	public ItemStack processing;
+	public ItemStack[] processing = new ItemStack[INVENTORY_SIZE];
 
     @Override
     public void readCustomNBT(NBTTagCompound nbt) {
@@ -34,19 +38,20 @@ public class TileSieveTable extends TileCore {
 	@Override
 	public void updateEntity() {
 		if (!worldObj.isRemote) {
-			if (processing == null) {
-				AxisAlignedBB scan = AxisAlignedBB.getBoundingBox(0, 1, 0, 1, 2, 1).offset(xCoord, yCoord, zCoord);
-				List entities = worldObj.getEntitiesWithinAABB(EntityItem.class, scan);
+			// Collect items
+			AxisAlignedBB scan = AxisAlignedBB.getBoundingBox(0, 1, 0, 1, 2, 1).offset(xCoord, yCoord, zCoord);
+			List entities = worldObj.getEntitiesWithinAABB(EntityItem.class, scan);
 
-				if (entities != null && entities.size() > 0) {
-					EntityItem item = (EntityItem) entities.get(0);
-					if (item.getEntityItem() != null) {
-						SieveManager.SieveRecipe recipe = RecipeManager.INSTANCE.sieve.get(item.getEntityItem());
+			if (entities != null && entities.size() > 0) {
+				EntityItem item = (EntityItem) entities.get(0);
+				if (item.getEntityItem() != null) {
+					SieveManager.SieveRecipe recipe = RecipeManager.INSTANCE.sieve.get(item.getEntityItem());
 
-						if (recipe != null) {
-							processingRecipe = recipe;
-							processing = item.getEntityItem().copy();
-							processing.stackSize = 1;
+					if (recipe != null) {
+						ItemStack stack = item.getEntityItem().copy();
+						stack.stackSize = 1;
+
+						if (TileEntityHopper.func_145889_a(this, stack, 1) == null) {
 							item.getEntityItem().stackSize--;
 
 							if (item.getEntityItem().stackSize <= 0) {
@@ -55,17 +60,131 @@ public class TileSieveTable extends TileCore {
 						}
 					}
 				}
-			} else {
-				ItemStack[] output = processingRecipe.getOutput();
-				Random random = new Random();
+			}
 
-				for (ItemStack out : output) {
-					InventoryHelper.ejectItem(worldObj, xCoord, yCoord - 1, zCoord, ForgeDirection.DOWN, out, random);
+			// Process items
+			//TODO Implement time
+			for (int i=0; i<processing.length; i++) {
+				ItemStack processed = processing[i];
+
+				if (processed != null && processed.stackSize > 0) {
+					SieveManager.SieveRecipe processingRecipe = RecipeManager.INSTANCE.sieve.get(processed);
+					ItemStack[] output = processingRecipe.getOutput();
+					Random random = new Random();
+
+					for (ItemStack out : output) {
+						InventoryHelper.ejectItem(worldObj, xCoord, yCoord, zCoord, ForgeDirection.DOWN, out, random);
+					}
+
+					processed.stackSize--;
+					if (processed.stackSize <= 0) {
+						processing[i] = null;
+					}
+
+					break;
 				}
-
-				processingRecipe = null;
-				processing = null;
 			}
 		}
 	}
+
+	/* IINVENTORY / ISIDEDINVENTORY */
+	@Override
+	public int getSizeInventory() {
+		return INVENTORY_SIZE;
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int slot) {
+		return processing[slot];
+	}
+
+	@Override
+	public ItemStack decrStackSize(int slot, int amt) {
+		if (processing[slot] != null) {
+			ItemStack itemstack;
+
+			if (processing[slot].stackSize <= amt) {
+				itemstack = processing[slot];
+				processing[slot] = null;
+				return itemstack;
+			} else {
+				itemstack = processing[slot].splitStack(amt);
+
+				if (processing[slot].stackSize == 0) {
+					processing[slot] = null;
+				}
+
+				return itemstack;
+			}
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int slot) {
+		if (processing[slot] != null) {
+			ItemStack itemstack = processing[slot];
+			processing[slot] = null;
+			return itemstack;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void setInventorySlotContents(int slot, ItemStack stack) {
+		processing[slot] = stack;
+	}
+
+	@Override
+	public String getInventoryName() {
+		return null;
+	}
+
+	@Override
+	public boolean hasCustomInventoryName() {
+		return false;
+	}
+
+	@Override
+	public int getInventoryStackLimit() {
+		return 64;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer player) {
+		return true;
+	}
+
+	@Override
+	public void openInventory() {
+
+	}
+
+	@Override
+	public void closeInventory() {
+
+	}
+
+	@Override
+	public boolean isItemValidForSlot(int slot, ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public int[] getAccessibleSlotsFromSide(int side) {
+		return side == 1 ? new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8} : new int[0];
+	}
+
+	@Override
+	public boolean canInsertItem(int slot, ItemStack stack, int side) {
+		return side == 1 && RecipeManager.INSTANCE.sieve.get(stack) != null;
+	}
+
+	@Override
+	public boolean canExtractItem(int slot, ItemStack var2, int side) {
+		return false;
+	}
+
 }
--- src/main/java/com/edxmod/electrodynamics/common/util/InventoryHelper.java
+++ src/main/java/com/edxmod/electrodynamics/common/util/InventoryHelper.java
@@ -1,8 +1,11 @@
 package com.edxmod.electrodynamics.common.util;
 
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityHopper;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -15,17 +18,30 @@ public class InventoryHelper {
 
 	public static void ejectItem(World world, int x, int y, int z, ForgeDirection side, ItemStack item, Random random) {
 		if (item != null) {
-			double spawnX = x + 0.5D + 0.5D * side.offsetX;
-			double spawnY = y + 0.5D + 0.5D * side.offsetY;
-			double spawnZ = z + 0.5D + 0.5D * side.offsetZ;
-			EntityItem entity = new EntityItem(world, spawnX, spawnY, spawnZ, item);
+			TileEntity tile = world.getTileEntity(x + side.offsetX, y + side.offsetY, z + side.offsetZ);
 
-//			if (item.hasTagCompound()) {
-//				entity.func_92059_d().func_77982_d((NBTTagCompound) item.func_77978_p().func_74737_b());
-//			}
-
-			world.spawnEntityInWorld(entity);
+			if (tile != null && tile instanceof IInventory) {
+				ItemStack result = TileEntityHopper.func_145889_a((IInventory) tile, item, side.getOpposite().ordinal());
+				if (result != null) {
+					dropItem(world, x, y, z, side, result, random);
+				}
+			} else {
+				dropItem(world, x, y, z, side, item, random);
+			}
 		}
+	}
+
+	public static void dropItem(World world, int x, int y, int z, ForgeDirection side, ItemStack stack, Random random) {
+		double spawnX = x + 0.5D + 1.5D * side.offsetX;
+		double spawnY = y + 0.5D + 1.5D * side.offsetY;
+		double spawnZ = z + 0.5D + 1.5D * side.offsetZ;
+		EntityItem entity = new EntityItem(world, spawnX, spawnY, spawnZ, stack);
+
+		if (stack.hasTagCompound()) {
+			entity.getEntityItem().setTagCompound((NBTTagCompound) stack.getTagCompound().copy());
+		}
+
+		world.spawnEntityInWorld(entity);
 	}
 
 }
