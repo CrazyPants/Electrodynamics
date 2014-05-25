7d760689b3a4629e0d094f1947cd745edc8443c3
dmillerw <dmillerw@gmail.com>
2014-05-24 19:46:59 -0700

    Rest of the machine core classes

---
+++ src/main/java/com/edxmod/electrodynamics/common/block/machine/BlockHammerMill.java
@@ -0,0 +1,32 @@
+package com.edxmod.electrodynamics.common.block.machine;
+
+import com.edxmod.electrodynamics.common.block.prefab.item.EDXTileBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+/**
+ * @author dmillerw
+ */
+public class BlockHammerMill extends EDXTileBlock {
+
+	public BlockHammerMill() {
+		super(Material.iron);
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World world, int meta) {
+		return null;
+	}
+
+	@Override
+	public boolean useCustomRender() {
+		return true;
+	}
+
+	@Override
+	public void registerBlockIcons(IIconRegister iconRegister) {
+
+	}
+}
