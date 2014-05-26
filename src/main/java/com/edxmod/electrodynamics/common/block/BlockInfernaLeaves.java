f350f5d3d49b8cbb633553ee4c391717d23a121b
dmillerw <dmillerw@gmail.com>
2014-05-26 13:26:48 -0700

    Fixed leaf bugs

--- src/main/java/com/edxmod/electrodynamics/common/block/BlockInfernaLeaves.java
+++ src/main/java/com/edxmod/electrodynamics/common/block/BlockInfernaLeaves.java
@@ -4,10 +4,13 @@ import com.edxmod.electrodynamics.common.core.EDXCreativeTab;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.block.BlockLog;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
+
+import java.util.Random;
 
 /**
  * @author dmillerw
@@ -31,8 +34,18 @@ public class BlockInfernaLeaves extends BlockLeaves {
 	}
 
 	@Override
+	public Item getItemDropped(int i1, Random random, int i2) {
+		return Item.getItemFromBlock(EDXBlocks.infernalSapling);
+	}
+
+	@Override
 	public boolean isOpaqueCube() {
 		return false;
+	}
+
+	@Override
+	public boolean shouldSideBeRendered(IBlockAccess p_149646_1_, int p_149646_2_, int p_149646_3_, int p_149646_4_, int p_149646_5_) {
+		return true;
 	}
 
 	@Override
