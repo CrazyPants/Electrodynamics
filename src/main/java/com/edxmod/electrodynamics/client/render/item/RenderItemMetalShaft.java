dbae6d0543ae98e128febd789647aee0faffffee
dmillerw <dmillerw@gmail.com>
2014-07-12 16:04:47 -0700

    Added missing files

---
+++ src/main/java/com/edxmod/electrodynamics/client/render/item/RenderItemMetalShaft.java
@@ -0,0 +1,40 @@
+package com.edxmod.electrodynamics.client.render.item;
+
+import com.edxmod.electrodynamics.client.lib.Model;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.IItemRenderer;
+import org.lwjgl.opengl.GL11;
+
+/**
+ * @author dmillerw
+ */
+public class RenderItemMetalShaft implements IItemRenderer {
+
+	@Override
+	public boolean handleRenderType(ItemStack item, ItemRenderType type) {
+		return true;
+	}
+
+	@Override
+	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
+		return true;
+	}
+
+	@Override
+	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+		GL11.glPushMatrix();
+
+		if (type == ItemRenderType.ENTITY) {
+			GL11.glTranslated(-0.5, 0, -0.5);
+		}
+
+		if (type == ItemRenderType.INVENTORY) {
+			GL11.glTranslated(0.1, 0, 0.1);
+		}
+
+		Model.METAL_SHAFT.bindTexture();
+		Model.METAL_SHAFT.renderAll();
+
+		GL11.glPopMatrix();
+	}
+}
